5ccf7ad8b4ae46ee22bf3dcd1c88ae364c4ae330
zakicode19 <zakicode19@gmail.com>
2025-03-25 01:53:44 +0100

    add visual class

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+	clock = pygame.time.Clock()
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill((0, 0, 0))
+		screen.fill("black")
 		pygame.display.flip()
-	
+
+		# compute FPS
+		dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
 	main()
