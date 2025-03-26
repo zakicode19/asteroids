d12faea6a3df44707dd737e7b2de086288c91768
zakicode19 <zakicode19@gmail.com>
2025-03-26 07:23:35 +0100

    add player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
 	print("Starting Asteroids!")
@@ -10,12 +10,24 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
+		updatable.update(dt)
+
 		screen.fill("black")
-		pygame.display.flip()
+
+		for draw_obj in drawable:
+			draw_obj.draw(screen)
+
+		pygame.display.flip() 
 
 		# compute FPS
 		dt = clock.tick(60)/1000
