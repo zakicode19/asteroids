2dfd2f9bc5a5a78cf32d6b06d0015d610ea50a00
zakicode19 <zakicode19@gmail.com>
2025-03-27 17:29:49 +0100

    add asterod field

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x,y, radius):
+        super().__init__(x,y, radius)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white",self.position, self.radius,2)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
 	print("Starting Asteroids!")
@@ -13,8 +16,14 @@ def main():
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable, )
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
